22bd1a35683aa1c885f036e545ae5d5a6b38d530
sudeep <sudeep000308@gmail.com>
2023-04-16 14:44:05 +0530

    constructor injection checked

--- src/main/java/sdapp/Car.java
+++ src/main/java/sdapp/Car.java
@@ -3,16 +3,17 @@ package sdapp;
 public class Car {
 
 	private Iengine eng;
-	public Car() {
-		System.out.println("car constructor no arguments");
-	}
+//	public Car() {
+//		System.out.println("car constructor no arguments");
+//	}
 	public Car(Iengine eng) {
 		System.out.println("car constructor");
 		this.eng = eng;
 	}
-	public void setEng(Iengine eng) {
-		this.eng = eng;
-	}
+//	public void setEng(Iengine eng) {
+//		System.out.println("setting engine");
+//		this.eng = eng;
+//	}
 	public void drivecar() {
 		int sd = eng.startengine();
 		if(sd==1) {
